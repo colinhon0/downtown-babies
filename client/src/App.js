c887a20d6f80848deed975ea1bce63e18b9dbbde
colinhon0 <colinhon0@gmail.com>
2021-09-11 18:44:29 +1000

    remove detection from app

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Detection from "./components/detection";
 
 function App() {
-  return (<div className="App"><Detection /></div>);
+  return (<div className="App"></div>);
 }
 
 export default App;
